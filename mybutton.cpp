b40fb5cc48d97f9e42e6795a5682070d3a2c2019
defabdullah <ahmet.susuz@boun.edu.tr>
2022-05-25 21:06:41 +0300

    add border color to buttons

--- mybutton.cpp
+++ mybutton.cpp
@@ -4,6 +4,10 @@ MyButton::MyButton(const QString& text,
                          QWidget* parent):
     QPushButton(text, parent)
 {
+    setAutoFillBackground(true);
+    QPalette palette = this->palette();
+    palette.setColor(QPalette::Window, QColor(Qt::green));
+    setPalette(palette);
     this->text=text;
     btsize = QSize(150, 100);
     setFixedSize(btsize);
